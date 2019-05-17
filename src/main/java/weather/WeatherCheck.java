eef22a459435039a02ce45b1d1edf40d2b717645
anisely <37018035+anisely@users.noreply.github.com>
2019-05-17 21:41:38 +0200

    First Commit

---
+++ src/main/java/weather/WeatherCheck.java
@@ -0,0 +1,42 @@
+package weather;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WeatherCheck
+{
+	//public static void main(String [] args)
+	public static void allCities()
+	{
+		String inline = "";
+	
+		try
+		{
+			URL url = new URL("http://api.openweathermap.org/data/2.5/group?id=6695624,7531002,3094802,3081368,7530858,3093133&units=metric&APPID=80ee1a399d66c8500b8cffa7f9846a94");
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.connect();
+			int responsecode = conn.getResponseCode();
+			System.out.println("Response code is: " +responsecode);
+			
+			if(responsecode != 200)
+				throw new RuntimeException("HttpResponseCode: " +responsecode);
+			else
+			{
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext())
+				{
+					inline+=sc.nextLine();
+				}
+				System.out.println(inline);
+				sc.close();
+			}
+			conn.disconnect();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
